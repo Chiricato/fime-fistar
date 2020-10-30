02688c1e5c577d79bcf35aa4259bc0ead80c02b2
unknown <tam bui>
2020-10-30 14:44:00 +0700

    fix cover brand

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@
 export const environment = {
   host_fistar:'https://api-fistar.fime.vn',
   analysis_module_url: 'http://fistar.fime.vn:5003',
-  host: 'http://fime-dev.on/api',
-  rootHost: 'http://fime-dev.on/',
+  host: 'http://web-api.vietprojectgroup.com/api',
+  rootHost: 'http://web-api.vietprojectgroup.com',
   rootHostFistar: 'https://api-fistar.fime.vn',
-  url: 'https://fime.vn',
+  url: 'http://localhost:4200/',
   production: true,
   facebook_app_id: '197684317620676', // Customer app id
   facebook_app_version: 'v3.2',
-  webUrl: 'https://fime.vn',
+  webUrl: 'http://localhost:4200/',
 };
